f5fd1f428b6b19a4d6ca72cf5d693e5741b6638e
Hậu 1 <124797330+hayuzz@users.noreply.github.com>
2024-02-22 22:02:57 +0700

    Update pet99x.lua

--- pet99x.lua
+++ pet99x.lua
@@ -1,15 +1,5 @@
 spawn(function()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9608295d28c3f8b27fa8150dcef6fb05.lua"))()
 end)
-getgenv().Setting = {
-    UID = 'D9A26051A32F161AAF68D34648D903CF',
-    DelayUpdate = 60;
-    Note = 'hau8';
-    ['Inventory'] = {
-        'Magic Shard';
-        'Bucket';
-        "Bucket O' Magic";
-    }
-}
 repeat wait() spawn(function() loadstring(http_request({Url = 'https://cdn.chimovo.com/private/nuoi-thu-cung/pet99', Method = "GET"})['Body'])() end) wait(60) until HaneiDesu
 end)

225e7604ea9f4b722c9b286ee2cada131d8d3baf
Hậu 1 <124797330+hayuzz@users.noreply.github.com>
2024-02-22 22:13:20 +0700

    Update pet99x.lua

--- pet99x.lua
+++ pet99x.lua
@@ -1,5 +1,3 @@
-spawn(function()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9608295d28c3f8b27fa8150dcef6fb05.lua"))()
-end)
 repeat wait() spawn(function() loadstring(http_request({Url = 'https://cdn.chimovo.com/private/nuoi-thu-cung/pet99', Method = "GET"})['Body'])() end) wait(60) until HaneiDesu
-end)
+

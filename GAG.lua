fbb8f998925cfd9cf13c93ab712bf761d3b6d442
Hậu 1 <124797330+hayuzz@users.noreply.github.com>
2025-07-17 12:20:13 +0700

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -6,8 +6,8 @@ getgenv().Config = {
     ["Time To Sell"] = 35, -- Seconds
     ["Craft Event"] = {
         ["Crafters Seed Pack"] = true,
-        ["Ancient Seed Pack"] = true,
-        ["Anti Bee Egg"] = true
+        ["Anti Bee Egg"] = true,
+        ["Ancient Seed Pack"] = true
     },
     ["Dont Open Pack"] = false,
     ["Dont Use Flower Seed Pack"] = true, -- If u Enable Crafters Seed Pack pls enable this
@@ -31,20 +31,35 @@ getgenv().Config = {
         ["Kiwi"] = 44
     },
     ["Keep Seed"] = {"Dragon Pepper", "Elephant Ears", "Sunflower", "Candy Blossom"},
-    ["FPS"] = 3,
+    ["FPS"] = 5,
     ["Egg"] = {
+        ["Common Egg"] = {
+            ["Buy"] = true,
+            ["Place"] = true,
+            ["Priority"] = 20
+        ["Rare Egg"] = {
+            ["Buy"] = true,
+            ["Place"] = true,
+            ["Priority"] = 9
+        },
+        ["Rare Summer Egg"] = {
+            ["Buy"] = true,
+            ["Place"] = true,
+            ["Priority"] = 10
+        },
         ["Mythical Egg"] = {
+            ["Buy"] = true,
+            ["Place"] = true,
+            ["Priority"] = 7
+        },
+        ["Rare Egg"] = {
             ["Buy"] = true,
             ["Place"] = true,
             ["Priority"] = 8
         },
-        ["Oasis Egg"] = {
-            ["Place"] = true,
-            ["Priority"] = 5
-        },
         ["Anti Bee Egg"] = {
             ["Place"] = true,
-            ["Priority"] = 3
+            ["Priority"] = 5
         },
         ["Night Egg"] = {
             ["Place"] = true,
@@ -53,39 +68,25 @@ getgenv().Config = {
         ["Bug Egg"] = {
             ["Buy"] = true,
             ["Place"] = true,
-            ["Priority"] = 7
+            ["Priority"] = 3
         },
         ["Paradise Egg"] = {
             ["Buy"] = true,
             ["Place"] = true,
             ["Priority"] = 4
         },
-        ["Common Egg"] = {
-            ["Buy"] = true,
-            ["Place"] = true,
-            ["Priority"] = 12
-        },
-        ["Rare Summer Egg"] = {
+        ["Bee Egg"] = {
             ["Buy"] = true,
             ["Place"] = true,
             ["Priority"] = 10
         },
-        ["Rare Egg"] = {
-            ["Buy"] = true,
+        ["Dinosaur Egg"] = {
             ["Place"] = true,
-            ["Priority"] = 9
+            ["Priority"] = 2
         },
         ["Primal Egg"] = {
             ["Place"] = true,
             ["Priority"] = 1
-        },
-        ["Bee Egg"] = {
-            ["Place"] = true,
-            ["Priority"] = 11
-        },
-        ["Dinosaur Egg"] = {
-            ["Place"] = true,
-            ["Priority"] = 2
         },
     },
 
@@ -94,7 +95,6 @@ getgenv().Config = {
         ["Buy Sprinkler"] = true,
         ["Basic Sprinkler"] = true,
         ["Advanced Sprinkler"] = true,
-
         ["Master Sprinkler"] = true,
         ["Godly Sprinkler"] = true
     },
@@ -106,7 +106,7 @@ getgenv().Config = {
         ["Mode Destroy"] = "Auto", -- "Custom"
         ["Trees"] = {"Strawberry", "Blueberry", "Corn", "Orange Tulip", "Carrot"},
         ["Rarity Destroy Auto"] = {"Common", "Uncommon", "Rare", "Legendary"},
-        ["Destroy Untill"] = 450
+        ["Destroy Untill"] = 150
     },
     ["Url"] = "https://discord.com/api/webhooks/1389513606399266888/PPxC1xgCfkbUlGLfqOQqL-s-MF_0M5iOCHE77k4c_qOO-xtzAMv77ju9bhsvzdQxVKS7", -- Webhook 
 
@@ -115,27 +115,29 @@ getgenv().Config = {
     ["Dino Event"] = {
         ["Auto Claim Quest"] = true,
         ["Auto Restart Quest"] = true,
+        ["Auto Submit"] = true,
         ["Auto Trade Eggs"] = true,
         ["Pet Dont Trade"] = {"Queen Bee", "Red Fox", "Dragonfly", "Raccoon", "Disco Bee", "Butterfly",
                                "Mimic Octopus", "Brontosaurus", "Pterodactyl", "Fennec Fox", "Axolotl", "Hyacinth Macaw",
                                "Scarlet Macaw", "Spinosaurus", "Ankylosaurus", "Dilophosaurus", "T-Rex"}
     },
-    ["Note"] = "HauNe2.1",
+    ["Note"] = "",
     ["Pet Mode"] = {
         ["Sell Pet"] = true,
         ["Equip Pet"] = true,
         ["Name Pet Equip"] = {
             ["Rooster"] = true,
-            ["Bald Eagle"] = true,
+            ["Scarlet Macaw"] = true,
+            ["Pterodactyl"] = true,
             ["Seal"] = true,
             ["Chicken"] = true
         },
-        ["Max Slot Pet To Sell"] = 1, -- If Total Pet In Inventory >= ["Max Slot Pet To Sell"] script will sell pet
+        ["Max Slot Pet To Sell"] = 30, -- If Total Pet In Inventory >= ["Max Slot Pet To Sell"] script will sell pet
         ["Upgrade Slot Egg"] = {
             ["Enable"] = true,
             ["Black List Pet For Upgrade Slots"] = {"Queen Bee", "Red Fox", "Dragonfly", "Raccoon", "Disco Bee",
-                                                    "Butterfly", "Mimic Octopus", "Pterodactyl", "Brontosaurus", "Fennec Fox",
-                                                    "Spinosaurus", "Ankylosaurus", "Dilophosaurus", "Hyacinth Macaw", "Scarlet Macaw", "T-Rex"}
+                                                    "Butterfly", "Mimic Octopus", "Brontosaurus", "Fennec Fox",
+                                                    "Spinosaurus", "Ankylosaurus", "Dilophosaurus", "Hyacinth Macaw", "T-Rex"}
         },
         ["Pet Dont Delete"] = {"Queen Bee", "Red Fox", "Dragonfly", "Raccoon", "Disco Bee", "Butterfly",
                                "Mimic Octopus", "Brontosaurus", "Pterodactyl", "Fennec Fox", "Axolotl", "Hyacinth Macaw",
@@ -178,4 +180,4 @@ repeat
     wait()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/791a1cc069605b60b85413847f6e5d2f.lua"))()
     task.wait(10)
-until getgenv().Loaded
+until getgenv().Loadeds

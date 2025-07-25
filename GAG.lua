a0f5f784b37449b489d9f015e6de152e198b6e11
Hậu 1 <124797330+hayuzz@users.noreply.github.com>
2025-07-26 01:52:34 +0700

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -13,7 +13,6 @@ getgenv().gagConfig = {
     REDEEM_CODES = {},
     EXTRA_PET_SLOTS = 3,
     EXTRA_EGG_SLOTS = 3,
-    EXTRA_PET_EQUIP_SLOTS = 3,
     MUTATE_PET = { "Spinosaurus", "T-Rex", "Fennec Fox", "Butterfly", "Disco Bee", "Raccoon", "Dragonfly", "Mimic Octopus", "Dilophosaurus", "Seal", "Blood Kiwi", "Rooster" },  -- Pet Names
     OPEN_ALL_SEED_PACK = true,
     ADD_FRIEND = true,

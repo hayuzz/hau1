53a9853187541e7dcef4713f715d9c31f5e73917
Hậu 1 <124797330+hayuzz@users.noreply.github.com>
2025-07-28 16:41:00 +0700

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -37,7 +37,7 @@ getgenv().gagConfig = {
     KEEP_PETS_AGE = {},
 
      -- EQUIP_PETS Priority (Left -> Right)
-    EQUIP_PETS = { ["Starfish"] = 3, , "Bald Eagle", ["Capybara"] = 1, ["Seal"] = 4, ["Rooster"] = 2,},
+    EQUIP_PETS = { ["Starfish"] = 3, "Bald Eagle", ["Capybara"] = 1, ["Seal"] = 4, ["Rooster"] = 2,},
     USE_PETS_FOR_UPGRADE_SLOT = { "Starfish" },
     REMOVE_PET_MAX_UPGRADE = { "Capybara", "Starfish" },  -- Unequip from garden
 
